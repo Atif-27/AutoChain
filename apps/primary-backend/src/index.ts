0e2d9454d1baa1aeeb26cec2baf0680227b70e82
Atif Ali <atif276ali@gmail.com>
2025-07-27 20:18:27 +0530

    add: setup primary backend with boiler plate code

---
+++ apps/primary-backend/src/index.ts
@@ -0,0 +1,31 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+
+dotenv.config();
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+app.use(cookieParser());
+app.use(
+  cors({
+    credentials: true,
+    origin: process.env.CORS_ORIGIN,
+  }),
+);
+
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true }));
+
+
+
+
+
+app.use("/api/v1/user", userRouter);
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on PORT ${PORT}`);
+});
