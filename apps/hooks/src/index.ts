d87f6bd94defce2f48cce29e9835d9424e1ecae5
Atif Ali <atif276ali@gmail.com>
2025-07-27 15:07:26 +0530

    refactor: restructure webhook handling and integrate Prisma for database transactions

--- apps/hooks/src/index.ts
+++ apps/hooks/src/index.ts
@@ -1,13 +1,38 @@
-import express from 'express'
+import { prisma } from "./lib/prisma";
+import express from 'express';
 
-const app=express();
+const app = express();
+const PORT = process.env.PORT || 3002;
+app.use(express.json());
+app.post("/hooks/catch/:userId/:zapId", async (req, res) => {
+    const userId = req.params.userId;
+    const zapId = req.params.zapId;
+    const body = req.body;
+    console.log(body);
 
-app.post("/webhook/:userId/:zapId",(req,res)=>{
-    const userId=req.params.userId;
-    const zapId=req.params.zapId;
+    await prisma.$transaction(async tx => {
+        const run = await tx.zapRun.create({
+            data: {
+                zapId: zapId,
+                metadata: body
+            }
+        });
 
-
+        await tx.zapRunOutBox.create({
+            data: {
+                zapRunId: run.id
+            }
+        });
+    });
     // store in db a new trigger
 
     // push it into kafka/redis
-})
+
+    res.json({
+        message: "WEBHOOK run successfully"
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(`Hooks Server is listening on port ${PORT}`);
+});
--- apps/hooks/src/lib/prisma.ts
+++ apps/hooks/src/lib/prisma.ts
@@ -1,3 +1,3 @@
 import {prisma} from "@repo/db"
 
-console.log(prisma);
+export {prisma}
