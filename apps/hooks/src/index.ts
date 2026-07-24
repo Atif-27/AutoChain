aefc604b256126aa011c96dbd3e4fe3c0ad0818d
Atif Ali <atif276ali@gmail.com>
2026-07-25 00:41:23 +0530

    added dotenv

--- apps/hooks/src/index.ts
+++ apps/hooks/src/index.ts
@@ -1,6 +1,9 @@
 import axios from "axios";
 import { prisma } from "./lib/prisma";
 import express from 'express';
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3002;
